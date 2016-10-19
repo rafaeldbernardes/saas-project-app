ed64cffe5a901cf3e02cbe17cc5bfb37b51cfe8c
rafaeldelbel <rafaeldbernardes@gmail.com>
2016-10-18 23:47:11 -0200

    Initial commit create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SassProjectApp::Application.config.secret_key_base = 'abbed5bc5d95d3deeb755a9d4b88d5f31177ae3542f88174055beabb62111c999420fb756a3c2dcea7817b7fa34d95b9f197a6a32e89095bbcf37d027c4761f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SassProjectApp::Application.config.session_store :cookie_store, key: '_sass-project-app_session'
